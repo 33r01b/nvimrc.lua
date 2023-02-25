40ef1a2baab7950a9e670e3d65556f6bde6c5697
33r01b <33r01b@gmail.com>
2023-02-25 14:34:04 +0300

    initial commit

---
+++ lua/keymaps.lua
@@ -0,0 +1,43 @@
+local map = vim.api.nvim_set_keymap 
+
+-- Remap space as leader key
+map('', '<Space>', '<Nop>', {noremap = true})
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Copy selected text to clipboard
+map('v', 'y', '"+y', {noremap = true})
+
+-- Ctrl+C as Esc
+map('i', '<C-c>', '<Esc>', {noremap = true})
+
+-- Disable search highlight
+map('n', '<Leader>c', ':nohl<Bar>:echo<CR>', {noremap = true, silent = true})
+
+-- Fast move
+map('n', '<S-l>', '4l', {noremap = true})
+map('n', '<S-h>', '4h', {noremap = true})
+map('n', '<S-j>', '4j', {noremap = true})
+map('n', '<S-k>', '4k', {noremap = true})
+
+-- Fast windows jump
+map('n', '<C-l>', '<C-w>l', {noremap = true})
+map('n', '<C-h>', '<C-w>h', {noremap = true})
+map('n', '<C-j>', '<C-w>j', {noremap = true})
+map('n', '<C-k>', '<C-w>k', {noremap = true})
+
+-- Join two lines
+map('n', '<Leader>j', 'J', {noremap = true})
+
+-- Show tabs, spaces, and line endings
+map('n', '<Leader>l', ':set list!<CR>', {noremap = true, silent = true})
+
+-- Disable ZZ exit
+map('n', 'Z', '<Nop>', {noremap = true})
+map('n', 'ZZ', '<Nop>', {noremap = true})
+
+-- Swith to previous file
+map('n', '<Tab>', ':e#<CR>', {noremap = true, silent = true})
+
+-- Spell toggle
+map('n', '<F8>', ':setlocal spell! spelllang=en,ru<CR>', {noremap = true, silent = true})
---
+++ lua/settings.lua
@@ -0,0 +1,74 @@
+local opt = vim.opt
+local g = vim.g
+
+g.nocompatible = true
+g.nocp = true
+g.nospell = true
+
+opt.ai = true
+opt.autoindent = true
+opt.backspace = '2'
+opt.backupdir = os.getenv('HOME') .. '/.config/nvim/.backup/'
+opt.cin = true
+opt.cindent = true
+opt.clipboard =  'unnamedplus'
+opt.colorcolumn = '120'
+opt.completeopt = 'menuone,noselect'
+opt.cursorline = true
+opt.directory = os.getenv('HOME') .. '/.config/nvim/.swp/'
+opt.encoding = 'utf8'
+opt.expandtab = true
+opt.fileencodings = 'utf8,cp1251'
+opt.fillchars = 'fold:-,diff:·'
+opt.foldlevel = 1
+opt.foldlevelstart = 3
+opt.foldmethod = 'indent'
+opt.foldopen = 'block,hor,insert,jump,mark,percent,quickfix,search,tag,undo'
+opt.hidden = true
+opt.hlsearch = true
+opt.ignorecase = true
+opt.incsearch = true
+opt.laststatus = 2
+opt.lazyredraw = true
+opt.listchars = "eol:↲,tab:» ,trail:·,extends:<,precedes:>,conceal:┊,nbsp:␣"
+opt.mousehide = true
+opt.number = true
+opt.re = 1
+opt.regexpengine = 1
+opt.ruler = true
+opt.scrolloff = 10
+opt.shell= '/bin/zsh'
+opt.shiftwidth = 4
+opt.showcmd = true
+opt.smartcase = true
+opt.smartindent = true
+opt.smarttab = true
+opt.softtabstop = 4
+opt.spelllang = 'en,ru'
+opt.spellsuggest = 'best,9'
+opt.splitbelow = true
+opt.splitright = true
+opt.synmaxcol = 1500
+opt.tabstop = 4
+opt.timeoutlen = 100
+opt.ttimeoutlen = 0
+opt.ttyfast = true
+opt.undodir = os.getenv('HOME') .. '/.config/nvim/.undo/'
+opt.updatetime = 100
+
+table.insert(opt.diffopt, 'iwhite') -- Ignore whitespace whilst diffing
+table.insert(opt.path, '**')
+table.insert(opt.switchbuf, 'usetab')
+table.insert(opt.switchbuf, 'newtab')
+
+for k,v in pairs(opt.ssop) do
+    -- do not store folds
+    if v == 'folds' then
+	    table.remove(opt.ssop, k)
+    end
+
+    -- do not store global and local values in a session
+    if v == 'options' then
+	    table.remove(opt.ssop, k)
+    end
+end
