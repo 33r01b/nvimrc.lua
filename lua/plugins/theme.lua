6125da763808f3593f340f300c7ff1a9fb0e370a
33r01b <33r01b@gmail.com>
2023-03-23 21:32:41 +0300

    fix: bg transparency

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,4 +1,3 @@
-vim.cmd.colorscheme "catppuccin"
 
 vim.api.nvim_set_hl(0, 'ColorColumn', {bg='#262626'})
 vim.api.nvim_set_hl(0, 'CursorLine', {bg='#262626'})
@@ -9,10 +8,8 @@ vim.api.nvim_set_hl(0, 'lualine_c_normal', {bg='#262626'})
 
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
     transparent_background = true,
     term_colors = true,
 })
+
+vim.cmd.colorscheme "catppuccin"
