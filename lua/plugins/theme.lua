6c4073ec9257c713ed096a9aeb18420e421ba823
33r01b <33r01b@gmail.com>
2023-05-18 07:53:13 +0300

    fix: color line

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,4 @@
 
-vim.api.nvim_set_hl(0, 'ColorColumn', {bg='#262626'})
-vim.api.nvim_set_hl(0, 'CursorLine', {bg='#262626'})
-vim.api.nvim_set_hl(0, 'NvimTreeVertSplit', {bg='#262626'})
-vim.api.nvim_set_hl(0, 'NvimTreeNormal', {bg=none})
-vim.api.nvim_set_hl(0, 'lualine_c_normal', {bg=none})
-vim.api.nvim_set_hl(0, 'lualine_c_normal', {bg='#262626'})
-
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
     transparent_background = true,
@@ -13,3 +6,10 @@ require("catppuccin").setup({
 })
 
 vim.cmd.colorscheme "catppuccin"
+
+vim.api.nvim_set_hl(0, 'ColorColumn', {bg='#262626'})
+vim.api.nvim_set_hl(0, 'CursorLine', {bg='#262626'})
+vim.api.nvim_set_hl(0, 'NvimTreeVertSplit', {bg='#262626'})
+vim.api.nvim_set_hl(0, 'NvimTreeNormal', {bg=none})
+vim.api.nvim_set_hl(0, 'lualine_c_normal', {bg=none})
+vim.api.nvim_set_hl(0, 'lualine_c_normal', {bg='#262626'})
